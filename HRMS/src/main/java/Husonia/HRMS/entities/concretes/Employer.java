bd1da904a9b76ff3887fa5ce6af1c9dcc70a50c9
husokanus <46330028+husokanus@users.noreply.github.com>
2021-07-10 16:42:14 +0300

    Add files via upload

---
+++ HRMS/src/main/java/Husonia/HRMS/entities/concretes/Employer.java
@@ -0,0 +1,40 @@
+package Husonia.HRMS.entities.concretes;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Table(name="employers")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employer extends User{
+	
+	@Column(name="company_name")
+	private String companyName;
+	
+	@Column(name="phone_number")
+	private String phoneNumber;
+	
+	@Column(name="web_site")
+	private String webSite;
+	
+	@Column(name="web_site_mail")
+	private String webSiteMail;
+	
+	@JsonInclude()
+	@Transient
+	private String passwordCheck;
+}
